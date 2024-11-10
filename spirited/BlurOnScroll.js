4a8ec80d99ec1bed2e5499bc3b4d267d701fc841
agart337 <agart337@newschool.edu>
2024-11-09 21:49:45 -0500

    dates

--- spirited/BlurOnScroll.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function () {
-    let num = (window.scrollY / window.innerHeight) * 4;
-
-    document.querySelector('body').style.backdropFilter = `blur(${num}px)`
-})
